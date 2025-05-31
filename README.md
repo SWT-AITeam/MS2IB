d429d26530745048ba95e4092d482a24706f8261
lixiwang <350071532@qq.com>
2025-05-31 20:07:08 +0800

    update paper link

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # The source code of the paper "Multi-View Syntax-Semantics Information Bottleneck for Dependency-driven Relation Extraction"
+
+PyTorch code for "Multi-View Syntax-Semantics Information Bottleneck for Dependency-driven Relation Extraction".  For a description of the model and experiments, see our paper: https://doi.org/10.1016/j.eswa.2025.128207 (published at Expert Systems with Applications).
+
+### Setup
+
+### Requirements
+
+- Required
+  - Python 3.8+
+  - PyTorch (tested with version 1.11.0)
+  - transformers (+sentencepiece, e.g. with 'pip install transformers[sentencepiece]', tested with version 4.1.1)
+  - scikit-learn (tested with version 0.24.0)
+  - tqdm (tested with version 4.55.1)
+  - numpy (tested with version 1.17.4)
+
+## Citation
+
+If you use or extend our work, please cite our paper.
+
+```
+@article{SHE2025128207,
+title = {Multi-view syntax-semantics information bottleneck for dependency-driven relation extraction},
+journal = {Expert Systems with Applications},
+volume = {288},
+pages = {128207},
+year = {2025},
+issn = {0957-4174},
+doi = {https://doi.org/10.1016/j.eswa.2025.128207},
+url = {https://www.sciencedirect.com/science/article/pii/S0957417425018275},
+author = {Wei She and Xiwang Li and Linpu Lv and Youwei Wang and Honghui Dong and Zhao Tian}
+}
+```
--- run_sample.sh
+++ run_sample.sh
@@ -1,9 +1,7 @@
 #train
-python -u re_agcn_main.py --do_train --do_eval --task_name semeval --data_dir ./data/semeval/ --model_path ../../data/bert_model_large_uncased/  --dep_type local_global_graph --model_name RE_AGCN.SEVEVAL.BERT.L --do_lower_case  --IB True --beta1 1e-5 --beta2 1e-10 --tag large_ib_beta1_1e-5_beta2_1e-10 
+python -u re_ms2ib_main.py --do_train --do_eval --task_name semeval --data_dir ./data/semeval/ --model_path ../../data/bert_model_large_uncased/  --dep_type local_global_graph --model_name RE_AGCN.SEVEVAL.BERT.L --do_lower_case  --IB True --beta1 1e-5 --beta2 1e-10 --tag large_ib_beta1_1e-5_beta2_1e-10 
 
 
 
- nohup python -u re_agcn_main.py --do_train --do_eval --task_name semeval --data_dir ./data/semeval/ --model_path ../../data/bert_model_large_uncased/  --dep_type local_global_graph --model_name RE_AGCN.SEVEVAL.BERT.L --do_lower_case  --IB True --beta1 1e-7 --beta2 1e-10 --tag large_ib_beta1_1e-7_beta2_1e-10_cuda_3_secondary --num_train_epoch 1000 >  large_ib_beta1_1e-7_beta2_1e-10_cuda_3.log &
+ nohup python -u re_ms2ib_main.py --do_train --do_eval --task_name semeval --data_dir ./data/semeval/ --model_path ../../data/bert_model_large_uncased/  --dep_type local_global_graph --model_name RE_AGCN.SEVEVAL.BERT.L --do_lower_case  --IB True --beta1 1e-7 --beta2 1e-10 --tag large_ib_beta1_1e-7_beta2_1e-10_cuda_3_secondary --num_train_epoch 1000 >  large_ib_beta1_1e-7_beta2_1e-10_cuda_3.log &
 
-
-num_train_epochs
